794254d92553226225ef6bed8b90865a76696851
tomXGames <tommaso.peduzzi@gmx.ch>
2021-06-01 08:23:18 +0200

    commit

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ var map, heatmap;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat:  46.8007, lng:  8.2227 },
-    zoom: 1,
+    dissipating: true,
+    maxIntensity: 1000000000000,
+    radius: 5,
+    opacity: 0.9,
+    zoom: 8
   });
   fetch('public-transport-ch.geojson').then(function(response) {
     response.json().then(function(result) {
@@ -21,15 +25,16 @@ function initMap() {
             });
             location = new google.maps.LatLng(y / val.geometry.coordinates.length, x/ val.geometry.coordinates.length );
           break;
-          case val.geometry.type == "Polygon":  // type is polygon
+          case "Polygon":  // type is polygon
               var x = 0, y= 0;
+              console.log(val.geometry.coordinates)
               val.geometry.coordinates[0].forEach(coords => {
                 x += parseFloat(coords[0]);
                 y += parseFloat(coords[1]);
               });
               location = new google.maps.LatLng(y / val.geometry.coordinates[0].length, x/ val.geometry.coordinates[0].length );
           break;
-          case val.geometry.type == "MultiPolygon":  // Type is MultiPolygon
+          case "MultiPolygon":  // Type is MultiPolygon
             var x=0, y=0;
             val.geometry.coordinates.forEach(shape => {
               shape.forEach((points) => {
@@ -46,19 +51,20 @@ function initMap() {
             console.error("Couldn't deserialize following object: ", val)
           break;
         }
-        weight = 5;
+
+        weight = 1;
+        if(val.properties.highway = "bus_stop") weight = 2;
+        else if(val.properties.railway = "platform") weight = 10;
         return {location: location, weight: weight};
       });
-      
+
 
       heatmap = new google.maps.visualization.HeatmapLayer({
-        data: locations,
-        opac: 1,
-        
+        data: locations,        
       });
 
-
       heatmap.setMap(map);
+      heatmap.set("radius", heatmap.get("radius") ? null : 20);
     });
   });
 }
